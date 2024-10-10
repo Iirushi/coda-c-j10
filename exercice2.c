6059b1225e77a5d8d4b10dde8036947aa5dcc821
Iirushi <maxime41.sobczak@gmail.com>
2024-10-10 15:04:34 +0200

    exercice2.c

--- exercice2.c
+++ exercice2.c
@@ -3,20 +3,20 @@
 int main() {
     FILE *fichier;
     
-    // Ouvrir le fichier en mode écriture ("w" pour write)
+    
     fichier = fopen("exercice2.txt", "w");
     
-    // Vérifier si le fichier a été ouvert avec succès
+ 
     if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier\n");
         return 1;
     }
     
-    // Écrire les nouvelles lignes dans le fichier
+  
     fprintf(fichier, "Frêre Jacques, frêre Jacques\n");
     fprintf(fichier, "Dormez-vous, dormez-vous ?\n");
     
-    // Fermer le fichier
+    
     fclose(fichier);
     
     printf("Le contenu du fichier exercice2.txt a été modifié avec succès.\n");
