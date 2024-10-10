d784813dd75750be0b7b33df7d62c0cf48a4ba14
Iirushi <maxime41.sobczak@gmail.com>
2024-10-10 15:01:56 +0200

    exercice1.c

---
+++ exercice1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *fichier;
+    char ligne[100];  // Buffer pour stocker chaque ligne
+
+    fichier = fopen("exercice1.txt", "r");
+
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture du fichier\n");
+        return 1;
+    }
+
+    printf("Contenu du fichier exercice1.txt :\n");
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        printf("%s", ligne);
+    }
+
+    fclose(fichier);
+    return 0;
+}
